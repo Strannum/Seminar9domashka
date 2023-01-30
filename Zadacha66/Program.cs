8be6fc6f1c6a1a64031d609a1a40f2e32424928d
dmitriy <dima-dim@yandex.ru>
2023-01-30 23:09:39 +0300

    Задача 66 решена

--- Zadacha66/Program.cs
+++ Zadacha66/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.Clear();
+Console.WriteLine("Введите M");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+
+int SumOfInterval(int firstNum, int secondNum)
+{
+
+    int sum = 0;
+    if (firstNum == secondNum) return secondNum;
+    sum = firstNum + SumOfInterval(firstNum + 1, secondNum);
+    Console.WriteLine($"{firstNum} + {secondNum} = {sum}");
+    return sum;
+}
+
+Console.WriteLine(SumOfInterval(M, N));
