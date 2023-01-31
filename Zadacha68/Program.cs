26220fc2a2c943b08c335c4a575441a1e9eb730d
dmitriy <dima-dim@yandex.ru>
2023-01-31 23:19:51 +0300

    Задача 68 решена

--- Zadacha68/Program.cs
+++ Zadacha68/Program.cs
@@ -1,31 +1,15 @@
-﻿using System;
-namespace AckermannFunction
+﻿Console.Clear();
+Console.WriteLine("Введите M");
+int mA = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Введите N");
+int nA = int.Parse(Console.ReadLine());
+
+int Ackermann(int m, int n)
 {
-    class Program
-    {
-        static int Ackermann(int m, int n)
-        {
-            if (m == 0)
-            {
-                return n + 1;
-            }
-            else if (m > 0 && n == 0)
-            {
-                return Ackermann(m - 1, 1);
-            }
-            else
-            {
-                return Ackermann(m - 1, Ackermann(m, n - 1));
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            int m = int.Parse(Console.ReadLine());
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine(Ackermann(m, n));
-        }
-    }
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
 }
 
+Console.WriteLine(Ackermann(mA, nA));
